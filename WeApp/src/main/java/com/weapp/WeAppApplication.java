ae8462db4a8a841c8a0e7803776bbdf25cd95484
Amy Nguyen <nguyen47@miamioh.edu>
2020-10-16 05:27:21 +0700

    Initial check-in

---
+++ WeApp/src/main/java/com/weapp/WeAppApplication.java
@@ -0,0 +1,13 @@
+package com.weapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeAppApplication.class, args);
+	}
+
+}
