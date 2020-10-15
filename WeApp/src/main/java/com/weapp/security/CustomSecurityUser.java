279865732b9c31ac5364cce2beec8b1f3983741a
Amy Nguyen <nguyen47@miamioh.edu>
2020-10-16 05:32:53 +0700

    Add security package

---
+++ WeApp/src/main/java/com/weapp/security/CustomSecurityUser.java
@@ -0,0 +1,59 @@
+package com.weapp.security;
+
+import java.util.Set;
+
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.weapp.domain.User;
+
+public class CustomSecurityUser extends User implements UserDetails {
+
+	private static final long serialVersionUID = 891696168063611285L;
+	
+
+	public CustomSecurityUser() {}
+	
+	public CustomSecurityUser(User user) {
+		this.setAuthorities(user.getAuthorities());
+		this.setId(user.getId());
+		this.setName(user.getName());
+		this.setPassword(user.getPassword());
+		this.setUsername(user.getName());
+	}
+
+	@Override
+	public Set<Authority> getAuthorities() {
+		return this.getAuthorities();
+	}
+
+	@Override
+	public String getPassword() {
+		return this.getPassword();
+	}
+
+	@Override
+	public String getUsername() {
+		return this.getUsername(); 
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+
+}
