0db67bafa8efd7a17279154cd98961c2b1d6dd4c
kangx0224 <72941195+kangx0224@users.noreply.github.com>
2020-10-16 22:13:50 +0800

    Add files via upload

---
+++ WeApp/src/main/java/com/weapp/domain/Comment.java
@@ -0,0 +1,19 @@
+
+public class Comment {
+	private int commentId;
+	private String content;
+	
+	public String author(String RegisteredUser) {
+		return RegisteredUser;
+	}
+	
+	public void created() {
+		//TimeStamp
+		//Created Time
+	}
+	
+	public void lastEdit() {
+		//TimeStamp
+		//lastEdit Time
+	}
+}
---
+++ WeApp/src/main/java/com/weapp/domain/Forum.java
@@ -0,0 +1,20 @@
+
+public class Forum {
+	private int forumId;
+	private String title;
+	private String content;
+	
+	public String author(String RegisteredUser) {
+		return RegisteredUser;
+	}
+	
+	public void created() {
+		//TimeStamp
+		//Created Time
+	}
+	
+	public void lastModified() {
+		//The lastModified Date
+	}
+			
+}
