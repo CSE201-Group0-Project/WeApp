d0b54f873b99301feab538d04b2eb1211f575abe
Amy Nguyen <nguyen47@miamioh.edu>
2020-10-18 20:07:42 +0700

    Update Comment and Forum tables

--- WeApp/src/main/java/com/weapp/domain/Comment.java
+++ WeApp/src/main/java/com/weapp/domain/Comment.java
@@ -2,29 +2,65 @@ package com.weapp.domain;
 
 import java.util.*;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+@Entity
+@JsonIdentityInfo(generator=ObjectIdGenerators.IntSequenceGenerator.class, property="@id")
 public class Comment {
-	private int commentId;
+	private int id;
 	private String content;
+	private User user; 
+	private Forum forum; 
+	private Date createdDate;
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	public int getId() {
-		return commentId;
+		return id;
 	}
-	
+	public void setId(int id) {
+		this.id = id;
+	}
 	public String getContent() {
 		return content;
 	}
-	
-	public String author(String RegisteredUser) {
-		return RegisteredUser;
+	public void setContent(String content) {
+		this.content = content;
 	}
 	
-	public void created() {
-		//TimeStamp
-		//Created Time
+	@ManyToOne
+	@JsonIgnore
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
 	}
 	
-	public void lastEdit() {
-		//TimeStamp
-		//lastEdit Time
+	@ManyToOne
+	public Forum getForum() {
+		return forum;
 	}
+	public void setForum(Forum forum) {
+		this.forum = forum;
+	}
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+	@Override
+	public String toString() {
+		return "Comment [id=" + id + ", content=" + content + "]";
+	} 
+	
 }
--- WeApp/src/main/java/com/weapp/domain/Forum.java
+++ WeApp/src/main/java/com/weapp/domain/Forum.java
@@ -2,34 +2,72 @@ package com.weapp.domain;
 
 import java.util.*;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+@Entity
+@JsonIdentityInfo(generator=ObjectIdGenerators.IntSequenceGenerator.class, property="@id")
 public class Forum {
-	private int forumId;
+	private int id;
 	private String title;
-	private String content;
+	private String description;
+	private User user; 
+	private Set<Comment> comments = new HashSet<>(); 
+	private Date createdDate;
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	public int getId() {
-		return forumId;
+		return id;
 	}
-	
+	public void setId(int id) {
+		this.id = id;
+	}
 	public String getTitle() {
 		return title;
 	}
-	
-	public String getContent() {
-		return content;
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
 	}
 	
-	public String author(String RegisteredUser) {
-		return RegisteredUser;
+	@ManyToOne
+	@JsonIgnore
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
 	}
 	
-	public void created() {
-		//TimeStamp
-		//Created Time
+	@OneToMany(cascade=CascadeType.ALL, fetch=FetchType.LAZY, mappedBy="forum")
+	public Set<Comment> getComments() {
+		return comments;
 	}
-	
-	public void lastModified() {
-		//The lastModified Date
+	public void setComments(Set<Comment> comments) {
+		this.comments = comments;
 	}
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+
 			
 }
