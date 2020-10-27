2774d1bd9703aa882ea74835d7dfe768b7829272
Amy Nguyen <nguyen47@miamioh.edu>
2020-10-28 03:12:48 +0700

    User Repository Integration Test

---
+++ WeApp/src/test/java/com/weapp/repositories/UserRepositoryIntegrationTest.java
@@ -0,0 +1,42 @@
+package com.weapp.repositories;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.weapp.domain.User;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace=AutoConfigureTestDatabase.Replace.NONE)
+class UserRepositoryIntegrationTest {
+	
+	@Autowired
+    private TestEntityManager entityManager;
+ 
+    @Autowired
+    private UserRepository userRepository;
+	
+    @Test
+    public void whenFindByUsername_thenReturnUser() {
+        // given
+        User amy = new User();
+        amy.setUsername("amynguyen.tech@jojo.com");
+        entityManager.persist(amy);
+        entityManager.flush();
+     
+        // when
+        User found = userRepository.findByUsername(amy.getUsername());
+     
+        // then
+        assertThat(found.getUsername())
+          .isEqualTo(amy.getUsername());
+    }
+
+}
