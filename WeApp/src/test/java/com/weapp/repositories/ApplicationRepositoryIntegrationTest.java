7dff5083aa40b87aace1ac29371ca36e29b8d48a
Amy Nguyen <nguyen47@miamioh.edu>
2020-11-01 19:09:13 +0700

    Update for H2

--- WeApp/src/test/java/com/weapp/repositories/ApplicationRepositoryIntegrationTest.java
+++ WeApp/src/test/java/com/weapp/repositories/ApplicationRepositoryIntegrationTest.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.weapp.domain.Application;
