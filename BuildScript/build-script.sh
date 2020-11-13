dd69a7c5a3e29b8b82e53e6309c32c973549a3f6
kangx0224 <72941195+kangx0224@users.noreply.github.com>
2020-11-12 19:18:47 -0500

    Update build-script.sh

--- BuildScript/build-script.sh
+++ BuildScript/build-script.sh
@@ -1,1 +1,11 @@
-
+#!/bin/bash
+echo 'Running script'
+rm -rf WeApp
+mkdir WeApp
+git clone https://github.com/CSE201-Group0-Project/WeApp.git
+git pull
+echo 'Finished cloning'
+echo 'All Web Files:'
+ls WeApp/*.php
+echo 'All SQL Files:'
+ls WeApp/*.sql
