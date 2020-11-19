c742e6d85a5fd0b1af94e59955d28b62bed2dabb
Amy Nguyen <nguyen47@miamioh.edu>
2020-11-20 04:06:08 +0700

    Test login and logout

--- WeApp/src/test/java/com/weapp/web/ApplicationAdminControllerTest.java
+++ WeApp/src/test/java/com/weapp/web/ApplicationAdminControllerTest.java
@@ -1,7 +1,6 @@
 /**
- * Thanks Petri Kainulainen for the tutorial on configuration on Spring MVC framework unit test for controllers
+ * Thanks to Petri Kainulainen for the tutorial on configuration on Spring MVC framework unit test for controllers
  * https://www.petrikainulainen.net/programming/spring-framework/unit-testing-of-spring-mvc-controllers-normal-controllers/
- * 
  * */
 package com.weapp.web;
 
@@ -30,7 +29,7 @@ import org.springframework.web.context.WebApplicationContext;
 
 import static org.hamcrest.Matchers.*;
 import static org.mockito.Mockito.*;
- 
+
 
 import com.weapp.domain.Application;
 import com.weapp.domain.User;
@@ -44,70 +43,139 @@ import com.weapp.service.ApplicationService;
 @SpringBootTest
 @AutoConfigureMockMvc
 class ApplicationAdminControllerTest {
-	
+
 	/** The context. */
 	@Autowired
 	private WebApplicationContext context;
-	
-    /** The mock mvc. */
-    @Autowired
-    private MockMvc mockMvc;
-    
-    /** The app service. */
-    @MockBean
-    private ApplicationService appService; 
-    
-    /**
-     * Setup.
-     */
-    @Before
-    public void setup() {
-        mockMvc = MockMvcBuilders
-                .webAppContextSetup(context)
-                .apply(springSecurity())
-                .build();
-    }
 
-    /**
-     * Find all should add app entries to model and render app list view.
-     *
-     * @throws Exception the exception
-     */
-    @Test
-    @WithMockUser(username = "admin", password = "ad", roles = "ADMIN")
-    public void findAll_ShouldAddAppEntriesToModelAndRenderAppListView() throws Exception {
+	/** The mock mvc. */
+	@Autowired
+	private MockMvc mockMvc;
+
+	/** The app service. */
+	@MockBean
+	private ApplicationService appService; 
+
+	/**
+	 * Setup.
+	 */
+	@Before
+	public void setup() {
+		mockMvc = MockMvcBuilders
+				.webAppContextSetup(context)
+				.apply(springSecurity())
+				.build();
+	}
+
+	/**
+	 * Find all should add app entries to model and render app list view.
+	 *
+	 * @throws Exception the exception
+	 */
+	@Test
+	@WithMockUser(username = "admin", password = "ad", roles = "ADMIN")
+	public void findAll_ShouldAddAppEntriesToModelAndRenderAppListView() throws Exception {
 		User user = mock(User.class);
-    	
-        Application first = new Application(1, "One", "Number", user); 
-        
-        Application second = new Application(2, "Two", "Number", user); 
-        
-        when(appService.findAll()).thenReturn(Arrays.asList(first, second));
-        
- 
-        mockMvc.perform(get("/applications"))
-                .andExpect(status().isOk())
-                .andExpect(view().name("dashboard"))
-                .andExpect(model().attribute("applications", hasSize(2)))
-                .andExpect(model().attribute("applications", hasItem(
-                        allOf(
-                                hasProperty("id", is(1)),
-                                hasProperty("name", is("One")),
-                                hasProperty("category", is("Number")),
-                                hasProperty("user", is(user))
-                        )
-                )))
-                .andExpect(model().attribute("applications", hasItem(
-                        allOf(
-                                hasProperty("id", is(2)),
-                                hasProperty("name", is("Two")),
-                                hasProperty("category", is("Number")),
-                                hasProperty("user", is(user))
-                        )
-                )));
- 
-        verify(appService, times(1)).findAll();
-        verifyNoMoreInteractions(appService);
-    }
+
+		Application first = new Application(1, "One", "Number", user); 
+
+		Application second = new Application(2, "Two", "Number", user); 
+
+		when(appService.findAll()).thenReturn(Arrays.asList(first, second));
+
+
+		mockMvc.perform(get("/applications"))
+		.andExpect(status().isOk()) // Make sure that the HTTP status code 200 is returned.
+		.andExpect(view().name("dashboard")) // Make sure the return view is dashboard 
+		// Make sure that model attribute applications has two items in it.
+		.andExpect(model().attribute("applications", hasSize(2)))
+		// Make sure the model attribute applications contains the correct items
+		.andExpect(model().attribute("applications", hasItem(
+				allOf(
+						hasProperty("id", is(1)),
+						hasProperty("name", is("One")),
+						hasProperty("category", is("Number")),
+						hasProperty("user", is(user))
+						)
+				)))
+		.andExpect(model().attribute("applications", hasItem(
+				allOf(
+						hasProperty("id", is(2)),
+						hasProperty("name", is("Two")),
+						hasProperty("category", is("Number")),
+						hasProperty("user", is(user))
+						)
+				)));
+
+		// Verify that the findAll method is invoked only once by the mock service 
+		verify(appService, times(1)).findAll();
+		// Make sure that other methods of mock service is not called during this test
+		verifyNoMoreInteractions(appService);
+	}
+
+	/**
+	 * Find by keyword should add app entries to model and render app list view.
+	 *
+	 * @throws Exception the exception
+	 */
+	@Test
+	@WithMockUser(username = "admin", password = "ad", roles = "ADMIN")
+	public void findByKeyword_ShouldAddAppEntriesToModelAndRenderAppListView() throws Exception {
+		User user = mock(User.class);
+
+		Application first = new Application(1, "One", "Number", user); 
+
+		Application second = new Application(2, "Two", "Number", user); 
+
+		when(appService.findByKeywordForAdmin("number")).thenReturn(Arrays.asList(first, second));
+
+
+		mockMvc.perform(get("/applications")
+				.param("keyword", "number")) // Add the keyword request parameter to MockHttpServletRequest 
+		.andExpect(status().isOk()) // Make sure that the HTTP status code 200 is returned.
+		.andExpect(view().name("dashboard"))  // Make sure the return view is dashboard 
+		.andExpect(model().attribute("keyword", "number")) 
+		// Make sure that model attribute applications has two items in it.
+		.andExpect(model().attribute("applications", hasSize(2))) 
+		// Make sure the model attribute applications contains the correct items
+		.andExpect(model().attribute("applications", hasItem(
+				allOf(
+						hasProperty("id", is(1)),
+						hasProperty("name", is("One")),
+						hasProperty("category", is("Number")),
+						hasProperty("user", is(user))
+						)
+				)))
+		.andExpect(model().attribute("applications", hasItem(
+				allOf(
+						hasProperty("id", is(2)),
+						hasProperty("name", is("Two")),
+						hasProperty("category", is("Number")),
+						hasProperty("user", is(user))
+						)
+				)));
+
+		// Verify that the findByKeywordForAdmin method is invoked only once by the mock service 
+		verify(appService, times(1)).findByKeywordForAdmin("number");
+		// Make sure that other methods of mock service is not called during this test
+		verifyNoMoreInteractions(appService);
+	}
+
+
+	/**
+	 * Test get application request.
+	 *
+	 * @throws Exception the exception
+	 */
+	@Test
+	@WithMockUser(username = "testUser", password = "pass", roles = "USER")
+	void testGetApplicationRequest() throws Exception {		
+		mockMvc.perform(get("/applicationRequest")
+				.sessionAttr("app", new Application()))
+		.andExpect(view().name("request-application"))
+		.andExpect(status().isOk())
+		.andExpect(model().attribute("app", hasProperty("id", is(0))))
+		.andReturn();
+	}
 
 }
--- WeApp/src/test/java/com/weapp/web/LoginControllerTest.java
+++ WeApp/src/test/java/com/weapp/web/LoginControllerTest.java
@@ -1,10 +1,16 @@
 /*
- * 
+ * Thanks to Spring Security Reference 
+ * https://docs.spring.io/spring-security/site/docs/5.0.x/reference/html/test-mockmvc.html
  */
 package com.weapp.web;
 
+
 import static org.mockito.Mockito.mock;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.logout;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.unauthenticated;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
@@ -18,7 +24,6 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import com.weapp.domain.User;
 
-// TODO: Auto-generated Javadoc
 /**
  * The Class LoginControllerTest.
  */
@@ -26,10 +31,10 @@ import com.weapp.domain.User;
 @SpringBootTest
 @AutoConfigureMockMvc
 public class LoginControllerTest {
-	
-    /** The mock mvc. */
-    @Autowired
-    private MockMvc mockMvc;
+
+	/** The mock mvc. */
+	@Autowired
+	private MockMvc mockMvc;
 
 
 	/**
@@ -39,12 +44,12 @@ public class LoginControllerTest {
 	 */
 	@Test
 	void testGetLogin() throws Exception {
-        this.mockMvc.perform(get("/login"))
-	        		.andExpect(view().name("login"))
-	                .andExpect(status().isOk())
-	                .andReturn();
+		this.mockMvc.perform(get("/login"))
+		.andExpect(view().name("login"))
+		.andExpect(status().isOk())
+		.andReturn();
 	}
-	
+
 	/**
 	 * Test get register.
 	 *
@@ -54,10 +59,52 @@ public class LoginControllerTest {
 	void testGetRegister() throws Exception {
 		User user = mock(User.class);
 		this.mockMvc.perform(get("/register")
-					.flashAttr("user", user))
-					.andExpect(view().name("register"))
-			        .andExpect(status().isOk())
-			        .andReturn();
+				.flashAttr("user", user))
+		.andExpect(view().name("register"))
+		.andExpect(status().isOk())
+		.andReturn();
 	}
 
+	/**
+	 * Test if a user can login and logout.
+	 * 
+	 * Thanks to Spring Security Reference 
+	 * https://docs.spring.io/spring-security/site/docs/5.0.x/reference/html/test-mockmvc.html#securitymockmvcrequestbuilders
+	 *
+	 * @throws Exception the exception
+	 */
+	@Test 
+	void testIfAUserCanLoginandLogout() throws Exception {
+		mockMvc
+		.perform(formLogin("/login")
+				.user("admin")
+				.password("pass"));
+		
+		mockMvc
+        .perform(logout())
+        .andExpect(status().isFound())
+        .andExpect(redirectedUrl("/login?logout"));
+	}
+	
+	  /**
+  	 * Invalid login denied.
+  	 * Thanks to Spring Security Reference 
+  	 * https://docs.spring.io/spring-security/site/docs/5.0.x/reference/html/test-mockmvc.html#unauthenticated-assertion
+  	 * @throws Exception the exception
+  	 */
+  	@Test
+	  public void testInvalidLogin() throws Exception {
+		// Given invalid password redirect to login?error
+		// and verifies that no user is authenticated 
+	    mockMvc
+	            .perform(formLogin().password("invalid"))
+	            .andExpect(status().isFound())
+	            .andExpect(redirectedUrl("/login?error"))
+	            .andExpect(unauthenticated());
+	    
+	    // Test if login?error is direct to login view 
+	    mockMvc
+	            .perform(get("/login?error"))
+	            .andExpect(view().name("login"));
+	  }
 }
